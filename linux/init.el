685e58a71f242bb470ecce30ff8bd5706a169f5e
kunio okita <chocokunio@gmail.com>
2017-03-13 09:38:44 +0900

    change for emacs25.1

--- linux/init.el
+++ linux/init.el
@@ -1,4 +1,11 @@
 ;; package manager
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 
@@ -25,7 +32,7 @@
 ;; メニューバーを非表示
 (menu-bar-mode -1)
 ;; スクロールバーを非表示
-;;(scroll-bar-mode 0)
+(scroll-bar-mode 0)
 
 
 ;; C-h backspace
@@ -68,7 +75,7 @@
 (defadvice next-line
     (around next-line-speedup activate)
   (if (and (string= last-command 'next-line)
-     (interactively-p))
+     (called-interactively-p 'any))
       (progn
   (scroll-speedup-setspeed)
   (scroll-speedup-next-line 'next))
@@ -79,7 +86,7 @@
 (defadvice previous-line
     (around previous-line-speedup activate)
   (if (and (string= last-command 'previous-line)
-     (interactively-p))
+     (called-interactively-p 'any))
       (progn
   (scroll-speedup-setspeed)
   (scroll-speedup-next-line 'previous))
